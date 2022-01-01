1febbd347b6e5e3c2db8fc629371e5262c3d8dcb
Andres Rojas <andresrojas12498706@gmail.com>
2021-12-31 19:37:43 -0500

    Primer commit

---
+++ useFetch.js
@@ -0,0 +1,49 @@
+import { useEffect, useState, useRef } from "react"
+
+export const useFetch = (url) => {
+   
+    const isMounted = useRef(true);
+    
+    const [state, setstate] = useState({data: null, loading: false, error: null})
+
+useEffect(() => {
+   
+    return () => {
+
+      isMounted.current = false;
+    }
+  
+},[])
+   
+
+  useEffect( () => {
+
+      setstate({data: null, loading: true, error: null})   
+ 
+         fetch(url)
+              .then(resp => resp.json())     
+               .then(data => {
+
+               
+            
+
+                  setstate({
+                    loading: false,
+                    error: null,
+                    data 
+    
+    
+                   })
+
+                
+
+         
+
+              
+            })
+  
+            },[url])
+
+return state;
+
+}
